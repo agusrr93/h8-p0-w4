896549792df2d7ca80bcfa2cb352a5423ad8f6fc
agusrr93 <randhania93@gmail.com>
2018-07-11 10:15:14 +0700

    Update most frequent large number.js

--- most frequent large number.js
+++ most frequent large number.js
@@ -1,8 +1,33 @@
 function sorting(arrNumber)
 {
-    var k=arrNumber.sort()
-    return k.reverse()
+    //improve with bubble sort
+       
 
+   while(sum!=0)
+   { 
+    for(var i=0;i<arrNumber.length-1;i++)
+    {
+        var temp
+        if(arrNumber[i]<arrNumber[i+1])
+        {
+           temp=arrNumber[i]
+           arrNumber[i]=arrNumber[i+1]
+           arrNumber[i+1]=temp
+        }
+    }
+
+    var sum=0
+    for(var i=0;i<arrNumber.length;i++)
+    {
+        if(arrNumber[i]<arrNumber[i+1])
+        {
+            sum=sum+1
+        }
+    }
+   }
+  
+  
+  return arrNumber
 }
 
 function getTotal(arrNumber)
@@ -37,4 +62,6 @@ function mostFrequentLargestNumbers(arrNumber)
 console.log(mostFrequentLargestNumbers([2,8,4,6,8,5,8,4]))
 console.log(mostFrequentLargestNumbers([122,122,130,100,135,100,135,150]))
 console.log(mostFrequentLargestNumbers([1,1,1]))
+console.log(mostFrequentLargestNumbers([1,100,5]))
 console.log(mostFrequentLargestNumbers([]))
+
